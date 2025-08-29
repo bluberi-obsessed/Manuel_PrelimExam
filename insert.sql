21c6349299f4205c98ea0d9286cd4f21e6c59047
Hazel Aillson Manuel <qhamanuel@tip.edu.ph>
2025-08-29 08:22:45 +0000

    insert.sql

---
+++ insert.sql
@@ -0,0 +1,5 @@
+CREATE TABLE prelim_table(
+	First_name varchar(255) NOT NULL,
+	Last_name varchar(255) NOT NULL
+	) ENGINE=NVISAM DEFAULT CHARSET=utf8;
+	INSERT INTO prelim_table(First_name, Last_name) VALUES('Hazel', 'Manuel');
